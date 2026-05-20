d30022e3d86a9631733db6922d2e9a1e3217b4cd
Julius Breindl <juliusb1616@gmail.com>
2026-05-20 21:48:58 +0200

    test updating by index

--- python/dev/add_numbers.py
+++ python/dev/add_numbers.py
@@ -1,5 +1,9 @@
 import mini_numpy
 
-print(mini_numpy.PyVector([1, 2, 3]))
-print(mini_numpy.PyVector([1, 2, 3]) + mini_numpy.PyVector([1, 2, 3]))
+vec = mini_numpy.PyVector([1, 2, 3])
+print(vec)
+print(vec + vec)
 print(mini_numpy.PyVector([1.0, 2.0, 3.0]) + mini_numpy.PyVector([1.1, 2.2, 3.3]))
+
+vec[0] = 10
+print(vec)
